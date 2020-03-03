b56d7f5f895ee1e1d84f87731641927047f7ef87
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-03-03 19:16:04 +0000

    Redesign File Structure

--- src/app.js
+++ src/app.js
@@ -1,49 +1,152 @@
 console.log("JavaScript is Connected!");
 
-  const startTab = document.getElementById('start')
-  const endTab = document.getElementById('end')
-  window.onload = function() {
-    getLocation();
-    
-  }
-  
-  const getLocation = () => {
-    navigator.geolocation.getCurrentPosition(position => console.log(position));
-  }
-  
-  function initMap() { //stay in function declaration for map-related tasks.
-    const directionsService = new google.maps.DirectionsService;
-    const directionsDisplay = new google.maps.DirectionsRenderer;
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
-    });
-    directionsDisplay.setMap(map);
+/* Constants */
 
-    const onChangeHandler = function() {
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    startTab.addEventListener('change', onChangeHandler);
-    endTab.addEventListener('change', onChangeHandler);
-  }
+const startTab = document.getElementById('start')
+const endTab = document.getElementById('end')
 
-  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    directionsService.route({
-      origin: document.getElementById('start').value,
-      destination: document.getElementById('end').value,
-      travelMode: 'DRIVING'
-    }, function(response, status) {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed', status);
-        console.log(status)
-      }
-    });
+/*  DOM Elements */
+
+/* Function Declarations */
+  
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(position => {
+      const long = position.coords.longitude;
+      const lat = position.coords.latitude;
+      console.log("User Genera Location:", position)
+      // return position;
+  })
+}
+  
+function initMap() {
+
+  const directionsService = new google.maps.DirectionsService; // Creating the directions Service instance
+  const directionsDisplay = new google.maps.DirectionsRenderer; // Create Renderer to render directions line on map
+  const ourMap = new google.maps.Map(document.getElementById('map'), { // Our Map object
+    zoom: 15,
+    center: {lat: 41.85, lng: -87.65}
+    // center: {lat: 40.699765, lng: -73.941055}
+  });
+  
+  // const marker = new google.maps.Marker({
+  //       position: {lat: 41.85, lng: -87.65},
+  //       map: ourMap,
+  //       title: 'Im a bathroom!'
+  //     });
+      
+  directionsDisplay.setMap(ourMap); //
+  
+  const onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  startTab.addEventListener('change', onChangeHandler); //when changed recalc the distance(1st param) and then render the blue directions line(2nd param)
+  endTab.addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+directionsService.route({
+  origin: document.getElementById('start').value,
+  destination: document.getElementById('end').value,
+  travelMode: 'DRIVING'
+}, function(response, status) {
+  if (status === 'OK') {
+    directionsDisplay.setDirections(response);
+  } else {
+    window.alert('Directions request failed', status);
+    console.log(status)
   }
+});
+}
+
+
+function getBathrooms() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log("General Position:", position);
+            const long = position.coords.longitude;
+            const lat = position.coords.latitude;
+
+            const url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?per_page=10&unisex=true&lat=${lat}&lng=${long}`;
+
+            fetch(url)
+                .then(res => res.json())
+                .then(bathrooms => {
+                    return bathrooms.map(bathroom => {
+                        return {
+                            id: bathroom.id,
+                            name: bathroom.name,
+                            street: bathroom.street,
+                            city: bathroom.city,
+                            state: bathroom.state,
+                            latitude: bathroom.latitude,
+                            longitude: bathroom.longitude,
+                            approved: bathroom.approved,
+                            unisex: bathroom.unisex,
+                            description: bathroom.directions,
+                            comment: bathroom.comment
+                        }
+                    })
+                })
+        })
+    }
+}
+// function getBathrooms() ;
+// function getBathrooms() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(position => {
+//                 console.log('My General Position:', position);
+//                 const long = position.coords.longitude;
+//                 const lat = position.coords.latitude;
+
+//                 const url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?per_page=10&unisex=true&lat=${lat}&lng=${long}`;
+//                 fetch(url).then(bathrooms => bathrooms.json())
+//                 .then(bathrooms => bathrooms.map(bathroom => {
+//                         return {
+//                             id: bathroom.id,
+//                             name: bathroom.name,
+//                             street: bathroom.street,
+//                             city: bathroom.city,
+//                             state: bathroom.state,
+//                             latitude: bathroom.latitude,
+//                             longitude: bathroom.longitude,
+//                             approved: bathroom.approved,
+//                             unisex: bathroom.unisex,
+//                             description: bathroom.directions,
+//                             comment: bathroom.comment
+//                         }
+//                     })
+//                 }));
+//             }
+//         }
+//     }
+
+
+function printOutDirections() {
+  
+}
+
+function placemarkersonMAp() {
+  // Loop through api get bathroom lat and longs
+  // Create anon new marker assigned to our map
+  // Change Title 
+  
+  const arr = getBathrooms();
+  
+  arr.forEach(item => {
+    console.log(item)
+  });
   
   
+  const marker = new google.maps.Marker({
+    position: {lat: 41.85, lng: -87.65},
+    map: map, // our map object is literally called map.
+    title: 'Im a bathroom!'
+  });
+}
+     
+getBathrooms()
+  /*  Function Delcarations  -->  News + Relevant Info  */ 
   
-  
-  
-//   Use Bathroom API Below here;
+//Get Users Location 
+
+
