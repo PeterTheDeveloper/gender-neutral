d857e77c0b6f7c52571e3edd108b1b2ef537d4b1
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-03-03 19:42:23 +0000

    Functions reMade

--- src/app.js
+++ src/app.js
@@ -24,18 +24,9 @@ function initMap() {
     const directionsDisplay = new google.maps.DirectionsRenderer; // Create Renderer to render directions line on map
     const ourMap = new google.maps.Map(document.getElementById('map'), { // Our Map object
         zoom: 15,
-        center: {
-            lat: 41.85,
-            lng: -87.65
-        }
+        center: {lat: 41.85, lng: -87.65}
         // center: {lat: 40.699765, lng: -73.941055}
     });
-
-    // const marker = new google.maps.Marker({
-    //       position: {lat: 41.85, lng: -87.65},
-    //       map: ourMap,
-    //       title: 'Im a bathroom!'
-    //     });
 
     directionsDisplay.setMap(ourMap); //
 
@@ -61,67 +52,41 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     });
 }
 
-
-function getBathrooms() {
+function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             console.log("General Position:", position);
             const long = position.coords.longitude;
             const lat = position.coords.latitude;
-
-            const url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?per_page=10&unisex=true&lat=${lat}&lng=${long}`;
-
-            fetch(url)
-                .then(res => res.json())
-                .then(bathrooms => {
-                    return bathrooms.map(bathroom => {
-                        return {
-                            id: bathroom.id,
-                            name: bathroom.name,
-                            street: bathroom.street,
-                            city: bathroom.city,
-                            state: bathroom.state,
-                            latitude: bathroom.latitude,
-                            longitude: bathroom.longitude,
-                            approved: bathroom.approved,
-                            unisex: bathroom.unisex,
-                            description: bathroom.directions,
-                            comment: bathroom.comment
-                        }
-                    })
-                })
+            return {lat, long}
         })
     }
 }
-// function getBathrooms() ;
-// function getBathrooms() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(position => {
-//                 console.log('My General Position:', position);
-//                 const long = position.coords.longitude;
-//                 const lat = position.coords.latitude;
 
-//                 const url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?per_page=10&unisex=true&lat=${lat}&lng=${long}`;
-//                 fetch(url).then(bathrooms => bathrooms.json())
-//                 .then(bathrooms => bathrooms.map(bathroom => {
-//                         return {
-//                             id: bathroom.id,
-//                             name: bathroom.name,
-//                             street: bathroom.street,
-//                             city: bathroom.city,
-//                             state: bathroom.state,
-//                             latitude: bathroom.latitude,
-//                             longitude: bathroom.longitude,
-//                             approved: bathroom.approved,
-//                             unisex: bathroom.unisex,
-//                             description: bathroom.directions,
-//                             comment: bathroom.comment
-//                         }
-//                     })
-//                 }));
-//             }
-//         }
-//     }
+function getBathrooms() {
+    ourLocation = getLocation()
+    const url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?per_page=10&unisex=true&lat=${ourLocation[lat]}&lng=${ourLocation[long]}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(bathrooms => {
+            return bathrooms.map(bathroom => {
+                return {
+                    id: bathroom.id,
+                    name: bathroom.name,
+                    street: bathroom.street,
+                    city: bathroom.city,
+                    state: bathroom.state,
+                    latitude: bathroom.latitude,
+                    longitude: bathroom.longitude,
+                    approved: bathroom.approved,
+                    unisex: bathroom.unisex,
+                    description: bathroom.directions,
+                    comment: bathroom.comment
+                }
+            })
+        })
+}
 
 
 function printOutDirections() {
@@ -129,24 +94,18 @@ function printOutDirections() {
 }
 
 function placemarkersonMAp() {
-    // Loop through api get bathroom lat and longs
-    // Create anon new marker assigned to our map
-    // Change Title 
 
     const arr = getBathrooms();
 
     arr.forEach(item => {
-        console.log(item)
-    });
-
-
-    const marker = new google.maps.Marker({
-        position: {
-            lat: 41.85,
-            lng: -87.65
-        },
-        map: map, // our map object is literally called map.
-        title: 'Im a bathroom!'
+       const newMarker = new google.maps.Marker({
+           position: {
+               lat: item.latitude,
+               lng: item.longitude
+           },
+          map: ourMap,
+          title: item.description + item.comment
+       })
     });
 }
 
