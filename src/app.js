9664656494f2d181e0f076f26e3cd8b70630abe7
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-03-03 19:22:27 +0000

    Spacing Changed to 4

--- src/app.js
+++ src/app.js
@@ -8,54 +8,57 @@ const endTab = document.getElementById('end')
 /*  DOM Elements */
 
 /* Function Declarations */
-  
+
 function getLocation() {
-  navigator.geolocation.getCurrentPosition(position => {
-      const long = position.coords.longitude;
-      const lat = position.coords.latitude;
-      console.log("User Genera Location:", position)
-      // return position;
-  })
+    navigator.geolocation.getCurrentPosition(position => {
+        const long = position.coords.longitude;
+        const lat = position.coords.latitude;
+        console.log("User Genera Location:", position)
+            // return position;
+    })
 }
-  
+
 function initMap() {
 
-  const directionsService = new google.maps.DirectionsService; // Creating the directions Service instance
-  const directionsDisplay = new google.maps.DirectionsRenderer; // Create Renderer to render directions line on map
-  const ourMap = new google.maps.Map(document.getElementById('map'), { // Our Map object
-    zoom: 15,
-    center: {lat: 41.85, lng: -87.65}
-    // center: {lat: 40.699765, lng: -73.941055}
-  });
-  
-  // const marker = new google.maps.Marker({
-  //       position: {lat: 41.85, lng: -87.65},
-  //       map: ourMap,
-  //       title: 'Im a bathroom!'
-  //     });
-      
-  directionsDisplay.setMap(ourMap); //
-  
-  const onChangeHandler = function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  };
-  startTab.addEventListener('change', onChangeHandler); //when changed recalc the distance(1st param) and then render the blue directions line(2nd param)
-  endTab.addEventListener('change', onChangeHandler);
+    const directionsService = new google.maps.DirectionsService; // Creating the directions Service instance
+    const directionsDisplay = new google.maps.DirectionsRenderer; // Create Renderer to render directions line on map
+    const ourMap = new google.maps.Map(document.getElementById('map'), { // Our Map object
+        zoom: 15,
+        center: {
+            lat: 41.85,
+            lng: -87.65
+        }
+        // center: {lat: 40.699765, lng: -73.941055}
+    });
+
+    // const marker = new google.maps.Marker({
+    //       position: {lat: 41.85, lng: -87.65},
+    //       map: ourMap,
+    //       title: 'Im a bathroom!'
+    //     });
+
+    directionsDisplay.setMap(ourMap); //
+
+    const onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    startTab.addEventListener('change', onChangeHandler); //when changed recalc the distance(1st param) and then render the blue directions line(2nd param)
+    endTab.addEventListener('change', onChangeHandler);
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-directionsService.route({
-  origin: document.getElementById('start').value,
-  destination: document.getElementById('end').value,
-  travelMode: 'DRIVING'
-}, function(response, status) {
-  if (status === 'OK') {
-    directionsDisplay.setDirections(response);
-  } else {
-    window.alert('Directions request failed', status);
-    console.log(status)
-  }
-});
+    directionsService.route({
+        origin: document.getElementById('start').value,
+        destination: document.getElementById('end').value,
+        travelMode: 'DRIVING'
+    }, function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed', status);
+            console.log(status)
+        }
+    });
 }
 
 
@@ -122,31 +125,32 @@ function getBathrooms() {
 
 
 function printOutDirections() {
-  
+
 }
 
 function placemarkersonMAp() {
-  // Loop through api get bathroom lat and longs
-  // Create anon new marker assigned to our map
-  // Change Title 
-  
-  const arr = getBathrooms();
-  
-  arr.forEach(item => {
-    console.log(item)
-  });
-  
-  
-  const marker = new google.maps.Marker({
-    position: {lat: 41.85, lng: -87.65},
-    map: map, // our map object is literally called map.
-    title: 'Im a bathroom!'
-  });
+    // Loop through api get bathroom lat and longs
+    // Create anon new marker assigned to our map
+    // Change Title 
+
+    const arr = getBathrooms();
+
+    arr.forEach(item => {
+        console.log(item)
+    });
+
+
+    const marker = new google.maps.Marker({
+        position: {
+            lat: 41.85,
+            lng: -87.65
+        },
+        map: map, // our map object is literally called map.
+        title: 'Im a bathroom!'
+    });
 }
-     
+
 getBathrooms()
-  /*  Function Delcarations  -->  News + Relevant Info  */ 
-  
-//Get Users Location 
+    /*  Function Delcarations  -->  News + Relevant Info  */
 
-
+//Get Users Location
